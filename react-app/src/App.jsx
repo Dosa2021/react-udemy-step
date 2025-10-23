56c51d0f6fa76af3ec7fc1b6712c67e7959aadf6
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-10-23 15:45:48 +0900

    react-router導入

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useCallback } from 'react'
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
 import './App.css'
 import { ChildArea } from './ChildArea';
 import { InlineStyle } from './components/InlineStyle';
@@ -6,6 +7,9 @@ import { CssModules } from './components/CssModules';
 import { StyledJsx } from './components/StyledJsx';
 import { StyledComponents } from './components/StyledComponents';
 import { Emotion } from './components/Emotion';
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
 
 export default function App() {
   const [text, setText] = useState('');
@@ -22,7 +26,26 @@ export default function App() {
   const onClickClose = useCallback(() =>  setOpen(false), [setOpen]);
 
   return (
+    <BrowserRouter>
     <div className='App'>
+      <Link to='/'>Home</Link>
+      <br />
+      <Link to='/page1'>Page1</Link>
+      <br />
+      <Link to='/page2'>Page2</Link>
+      <br />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/page1'>
+          <Page1 />
+        </Route>
+        <Route path='/page2'>
+          <Page2 />
+        </Route>
+      </Switch>
+      <br />
       <InlineStyle />
       <br />
       <CssModules />
@@ -37,6 +60,7 @@ export default function App() {
       <button onClick={onClickOpen}>表示</button>
       <ChildArea open={open} onClickClose={onClickClose} />
     </div>
+    </BrowserRouter>
   )
 }
 
